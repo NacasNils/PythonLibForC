df541c88b9ab449f38b8f6db2b07d46f1ed5d79f
lyrevik <lyrevik@student.chalmers.se>
2025-02-24 23:34:12 +0100

    Added rgb functionality

--- ExtendPythonWithC/main.py
+++ ExtendPythonWithC/main.py
@@ -1,15 +1,16 @@
 import ctypes
 
-# Load the shared library
-myC = ctypes.CDLL("./myCfile.so")  # Use "myCfile.dll" on Windows
-myC.remove_color.argtypes = [ctypes.c_char_p]
+myC = ctypes.CDLL("./myCfile.so")  # .dll for WINDOWS
+myC.remove_color.argtypes = [ctypes.c_char_p, ctypes.c_int]
+
 filename = input("please enter filename: ")
+print("[0 for gray] -- [1 for red] -- [2 for green] -- [3 for blue]")
+mode = int(input("please enter [0,1,2,3]: "))
 
-filename_bytes = filename.encode('utf-8')
-result = myC.remove_color(filename_bytes)
+filename_bytes = filename.encode("utf-8")
+result = myC.remove_color(filename_bytes, mode)
 
-# Call the C function
 if result == 1:
     print("Success!")
-else: 
+else:
     print("failed somewhere in c-code...")
--- ExtendPythonWithC/myCfile.c
+++ ExtendPythonWithC/myCfile.c
@@ -6,18 +6,37 @@
 #include "stb_image/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+int remove_all(int width, int height, int channels, unsigned char *img);
+int remove_rgb(int width, int height, int channels, unsigned char *img,int color);
 
 
-
-int remove_color(const char *input){
+int remove_color(const char *input, int mode){
   int width,height,channels;
   unsigned char *img = stbi_load(input, &width, &height, &channels, 0);
 
   // ERROR HANDLING
   if(img == NULL){
     return -1;
-    exit(1);
   }
+  if(mode == 0){
+    return remove_all(width,height,channels,img);
+  }
+  if(mode == 1){
+    return remove_rgb(width,height,channels,img,RED);
+  }
+  if (mode == 2){
+    return remove_rgb(width, height, channels, img, GREEN);
+  }
+  if(mode == 3){
+    return remove_rgb(width,height, channels, img, BLUE);
+  }
+  return 2;
+}
+
+int remove_all(int width, int height, int channels, unsigned char *img){
 
   size_t img_size = width * height * channels;
   int gray_channels = channels == 4 ? 2 : 1;
@@ -26,8 +45,7 @@ int remove_color(const char *input){
 
   // ERROR HANDLING
   if(gray_img == NULL){
-    return -1;
-    exit(1);
+    return -2;
   }
 
   for (unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels){
@@ -36,11 +54,33 @@ int remove_color(const char *input){
       *(pg + 1) = *(p + 3);
     }
   }
-
-  stbi_write_png("./output.png", width, height, gray_channels, gray_img, width * gray_channels);
+  stbi_write_png("./out_gray.png", width, height, gray_channels, gray_img, width * gray_channels);
   
   
   stbi_image_free;
   free(gray_img);
+  return 1;
+}
+
+int remove_rgb(int width, int height, int channels, unsigned char *img, int color){
+  size_t img_size = width * height * channels;
+  size_t red_img_size = width * height * channels;
+
+  unsigned char *red_img = malloc(red_img_size);
+
+  if(red_img == NULL){
+    return -2;
+  }
+for (unsigned char *p = img, *pr = red_img; p != img + img_size; p += channels, pr += channels) {
+    // Copy full pixel (RGB or RGBA)
+    memcpy(pr, p, channels);  
+
+    // Set red channel to 0
+    pr[color] = 0;
+}
+
+  stbi_write_png("./out_rgb.png", width, height, channels, red_img, width*channels);
+  stbi_image_free;
+  free(red_img);
   return 1;
 }
