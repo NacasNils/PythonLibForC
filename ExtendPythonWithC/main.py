5e2ab7dcd796b8165f1805a8d9ee06964d243803
lyrevik <lyrevik@student.chalmers.se>
2025-02-24 15:20:52 +0100

    finally working

--- ExtendPythonWithC/main.py
+++ ExtendPythonWithC/main.py
@@ -2,8 +2,14 @@ import ctypes
 
 # Load the shared library
 myC = ctypes.CDLL("./myCfile.so")  # Use "myCfile.dll" on Windows
+myC.remove_color.argtypes = [ctypes.c_char_p]
 filename = input("please enter filename: ")
 
+filename_bytes = filename.encode('utf-8')
+result = myC.remove_color(filename_bytes)
+
 # Call the C function
-result = myC.remove_color(filename)
-print(result)
+if result == 1:
+    print("Success!")
+else: 
+    print("failed somewhere in c-code...")
--- ExtendPythonWithC/myCfile.c
+++ ExtendPythonWithC/myCfile.c
@@ -12,10 +12,35 @@
 int remove_color(const char *input){
   int width,height,channels;
   unsigned char *img = stbi_load(input, &width, &height, &channels, 0);
+
+  // ERROR HANDLING
   if(img == NULL){
+    return -1;
     exit(1);
   }
-  stbi_write_png("output.png",width,height,channels, img, width * channels);
+
+  size_t img_size = width * height * channels;
+  int gray_channels = channels == 4 ? 2 : 1;
+  size_t gray_img_size = width * height;
+  unsigned char *gray_img = malloc(gray_img_size);
+
+  // ERROR HANDLING
+  if(gray_img == NULL){
+    return -1;
+    exit(1);
+  }
+
+  for (unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels){
+    *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3);
+    if(channels == 4){
+      *(pg + 1) = *(p + 3);
+    }
+  }
+
+  stbi_write_png("./output.png", width, height, gray_channels, gray_img, width * gray_channels);
+  
   
   stbi_image_free;
+  free(gray_img);
+  return 1;
 }
