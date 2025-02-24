f57fe44bcee7bcc869d984053ee92d0655e04ec4
lyrevik <lyrevik@student.chalmers.se>
2025-02-24 14:04:58 +0100

    more done

--- ExtendPythonWithC/main.py
+++ ExtendPythonWithC/main.py
@@ -1,7 +1,9 @@
 import ctypes
 
 # Load the shared library
-myC = ctypes.CDLL("./myCudaFile.so")  # Use "myCfile.dll" on Windows
+myC = ctypes.CDLL("./myCfile.so")  # Use "myCfile.dll" on Windows
+filename = input("please enter filename: ")
 
 # Call the C function
-result = myC.add(3, 5)
+result = myC.remove_color(filename)
+print(result)
--- ExtendPythonWithC/myCfile.c
+++ ExtendPythonWithC/myCfile.c
@@ -1,4 +1,21 @@
-#include <stdio.>
-int add(int x,int y){
-  return x+y;
+#include <stdio.h>
+#include <stdlib.h>
+
+// stb_import
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image/stb_image_write.h"
+
+
+
+int remove_color(const char *input){
+  int width,height,channels;
+  unsigned char *img = stbi_load(input, &width, &height, &channels, 0);
+  if(img == NULL){
+    exit(1);
+  }
+  stbi_write_png("output.png",width,height,channels, img, width * channels);
+  
+  stbi_image_free;
 }
